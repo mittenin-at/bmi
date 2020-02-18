15dfa97ddea93ac1402668648fda1dcf6142e168
stefan Haslinger <mail@stefan-haslinger.at>
2020-02-18 17:47:29 +0100

    bootstrap 3 running

--- app/webpack/packs/application.js
+++ app/webpack/packs/application.js
@@ -3,6 +3,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("bootstrap-sass/assets/javascripts/bootstrap")
+
 import '../stylesheets/application'
+
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
