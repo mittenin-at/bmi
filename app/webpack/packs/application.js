a68ec06578f3735dde5aa5652463d37574eac240
stefan Haslinger <mail@stefan-haslinger.at>
2020-02-19 15:17:54 +0100

    nvd3 importet as .js-files

--- app/webpack/packs/application.js
+++ app/webpack/packs/application.js
@@ -1,8 +1,6 @@
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
-const glyphicons = require.context('../../../node_modules/bootstrap-sass/assets/fonts/bootstrap', true)
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -15,6 +13,10 @@ global.jQuery = $
 
 require("datatables.net/js/jquery.dataTables")
 require("datatables.net-bs/js/dataTables.bootstrap")
+
+import './nvd3/d3.v2'
+import './nvd3/fisheye'
+import './nvd3/nv.d3'
 
 require("bootstrap-sass/assets/javascripts/bootstrap")
 
